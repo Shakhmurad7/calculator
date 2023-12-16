3c6e7efa2e8133606f6a9f8189a1aad12a476a2e
Shakhmurad <muradb356@gmail.com>
2023-12-17 03:13:44 +0400

    add calculator

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,37 @@ const [count , setcount] = useState('')
     <>
       <div className="claculator-contaner">
           <div className="calculator-input">
-              <h2>{count}</h2>
+             <h2>{count}</h2>
           </div>
           <div className="calculator-text">
             <div className="calculator-block">
-              <button onClick={()=>setcount('0')} >AC</button>
+              <button onClick={()=>setcount('')} >AC</button>
               <button onClick={()=>setcount(count.slice(0 , -1))} >DL</button>
-              <button>X</button>
-              <button>/</button>
+              <button onClick={()=>setcount(count + '*')} >X</button>
+              <button  onClick={()=>setcount(count + '/')} >/</button>
             </div>
             <div className="calculator-block">
-              <button  onClick={()=>setcount('1' + count)} >1</button>
-              <button  onClick={()=>setcount('2' + count )}>2</button>
-              <button  onClick={()=>setcount('3')}>3</button>
-              <button>+</button>
+              <button  onClick={()=>setcount(count + '1')} >1</button>
+              <button  onClick={()=>setcount(count + '2' )}>2</button>
+              <button  onClick={()=>setcount(count + '3')}>3</button>
+              <button onClick={()=>setcount(count + '+')} >+</button>
             </div>
             <div className="calculator-block">
-              <button onClick={()=>setcount('4')} >4</button>
-              <button onClick={()=>setcount('5')} >5</button>
-              <button onClick={()=>setcount('6')} >6</button>
-              <button>-</button>
+              <button onClick={()=>setcount(count + '4')} >4</button>
+              <button onClick={()=>setcount(count + '5')} >5</button>
+              <button onClick={()=>setcount(count + '6')} >6</button>
+              <button onClick={()=>setcount(count + '-')}  >-</button>
             </div>
             <div className="calculator-block">
-              <button  onClick={()=>setcount('7')} >7</button>
-              <button  onClick={()=>setcount('8')} >8</button>
-              <button  onClick={()=>setcount('9')} >9</button>
-              <button>+</button>
+              <button  onClick={()=>setcount(count + '7')} >7</button>
+              <button  onClick={()=>setcount(count + '8')} >8</button>
+              <button  onClick={()=>setcount(count + '9')} >9</button>
+              <button onClick={()=> setcount(eval(count))} >=</button>
             </div>
             <div className="calculator-block-button">
-              <button  onClick={()=>setcount('0')} >0</button>
-              <button>%</button>
-              <button>.</button>
+              <button  onClick={()=>setcount(count + '0')} >0</button>
+              <button onClick={()=>setcount(count / 100)} >%</button>
+              <button onClick={()=>setcount(count + '.') } >.</button>
             </div>
           </div>
       </div>
